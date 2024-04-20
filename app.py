7b22ef85116f286038d66aceeec80bfb70c535b9
Jakob Willforss <jakob.willforss@hotmail.com>
2024-04-20 09:30:23 +0200

    Basic self updating timer

--- app.py
+++ app.py
@@ -2,11 +2,13 @@
 
 from flask import Flask, render_template
 app = Flask(__name__)
+import datetime
 
 @app.route("/")
 def hello():
     title = "Test title"
     users = ["Xuan", "Jakob", "Alvar"]
+    now = str(datetime.datetime.now().strftime("%Y-%m-%d %H:%M"))
     return render_template("app.html", **locals())
 
 @app.route("/user/")
